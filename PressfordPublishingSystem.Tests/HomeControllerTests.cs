1e645d7f1f52f909b360157de1568d56eefdac43
propes <not specified>
2017-03-28 21:37:55 +0100

    Set up solution. Add navbar and basic page structure. Add styles.

---
+++ PressfordPublishingSystem.Tests/HomeControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PressfordPublishingSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PressfordPublishingSystem.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void IndexReturnsPage()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void HelpReturnsPage()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Help();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void AboutReturnsPage()
+        {
+            var controller = new HomeController();
+
+            var result = controller.About();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
